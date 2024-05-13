75129b1cacec95d5845cad333f263f3ce67b5c61
cocka67 <jdanoffk@yandex.ru>
2024-05-13 20:24:37 +0500

    da

--- controllers/games.js
+++ controllers/games.js
@@ -5,5 +5,33 @@ const sendAllGames = (req, res) => {
     // Вернём найденные игры в формате JSON
     res.end(JSON.stringify(req.gamesArray));
   };
+
+  const sendGameCreated = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.game));
+  };
+
+  const sendGameById = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.game));
+  };
   
-  module.exports = sendAllGames;
+const sendGameUpdated = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end({ message: "Игра обновлена" });
+};
+
+const sendGameDeleted = (req, res) => {
+  // Объект игры отправляем в формате JSON 
+res.setHeader("Content-Type", "application/json");
+// Отправляем на клиент найденный и удалённый элемент из базы данных
+res.end(JSON.stringify(req.game));
+};
+
+  module.exports = {
+    sendAllGames, 
+    sendGameCreated,
+    sendGameById,
+    sendGameUpdated,
+    sendGameDeleted
+  };
