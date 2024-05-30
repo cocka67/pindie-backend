2c8f3725dd347df66ed9cbdd4fcb66b9c501fb2e
cocka67 <jdanoffk@yandex.ru>
2024-05-30 19:32:20 +0500

    lf

--- controllers/categories.js
+++ controllers/categories.js
@@ -19,7 +19,7 @@ const sendCategoryById = (req, res) => {
 
 const sendCategoryUpdated = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(req.category));
+  res.end(JSON.stringify({ message: 'Категория обновлена' }));
 };
 
 const sendCategoryDeleted = (req, res) => {
